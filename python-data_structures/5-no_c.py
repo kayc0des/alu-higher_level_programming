ae67eda2cf028da6403d2d63449b744ea8c138f1
kayc0des <k.boafo@alustudent.com>
2022-11-04 09:59:27 -0700

    translate string

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,4 +2,4 @@
 def no_c(my_string):
     my_string = my_string.translate({ord("c"): None})
     my_string = my_string.translate({ord("C"): None})
-    return 
+    return(my_string)(my_string)(my_string)(my_string)(my_string)(my_string)(my_string)(my_string)(my_string) 
