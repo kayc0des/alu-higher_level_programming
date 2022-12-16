adf6f6ca4626079c9a6c176bde0b8d809afd1227
kayc0des <k.boafo@alustudent.com>
2022-12-15 22:56:20 -0800

    fixed

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,23 +1,12 @@
+
 #!/usr/bin/python3
-'''a function that adds 2 integers.'''
+"""This script defines an addition function"""
 
 
 def add_integer(a, b=98):
-    ''' Function that adds two integers
-    Args:
-        a : this must be either an integer or float
-        b : Must be either an integer or float, and if not provided
-            it takes the defualt value of 98
-    Returns:
-        an integer: the addition of a and b
-    '''
-    if type(a) == float or type(b) == float:
-        a = int(a)
-        b = int(b)
-
-    if type(a) != int:
+    """This function adds two integers"""
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) != int:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
-    return a + b 
+    return int(a) + int(b)
